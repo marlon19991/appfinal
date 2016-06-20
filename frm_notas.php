9f9d2af14fb2c9e81876c89ddc5817b32f984cbf
marlon19991 <esteban19991@gmail.com>
2016-06-19 23:02:10 -0500

    select est

--- frm_notas.php
+++ frm_notas.php
@@ -42,12 +42,70 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
  
     
     $cursos=mysqli_query($conexion,$csreporte) or die("problemas en la consulta");
+
+    $registroe=mysqli_query($conexion,"SELECT * FROM usuario where id_tipo_usuario=2") or die ("Hay problemas en la consulta"); 
+    $registrop=mysqli_query($conexion,"SELECT * FROM Periodo ") or die ("Hay problemas en la consulta");
+    $registrom=mysqli_query($conexion,"SELECT * FROM materia ") or die ("Hay problemas en la consulta");
+    $registrog=mysqli_query($conexion,"SELECT * FROM grado ") or die ("Hay problemas en la consulta");
+    
+    
        ?>
        
 <!-- ojala y porfin deje subir esto -->
 
                 
      <div class="container">
+
+
+<div class="content">
+                <!--about-->
+                    <div class="about-section">
+                        <div class="container">
+                            <h2><span>Registrar</span> Nota</h2>
+                                <div class="about-grids">
+                                    <div class="col-md-12 about-grid1" align="center">
+                                        <img src="images/g8.jpg" width="200px" height="100px" class="img-responsive" alt="/">
+                                    </div>
+                                </div>
+                        </div>
+                    </div>
+                    <!--about-->
+                    <div class="cooking">
+                        <div class="container">
+                            <div class="cooking-grids">
+                                <div class="col-md-12 cook-grid">
+                                    <div class="cook3">
+                                        <h3>Formulario</h3>
+                                        <br>
+                                        <p>Completa los campor y registra las notas periodicas de los estudiantes</p>
+                                        <br>
+                                        
+                                            <form method="Post" action="registrar-nota.php">
+
+                                            <div class="row">
+                                                <div class="col-md-4">
+                                                    <div><label for="select-native-1">Estudiantes</label></div>
+                                                </div>
+                                                <div class="col-md-5">
+                                                          <select name="id_usuario" id="select-native-1" class="btn btn-warning">
+                                                              <option value="">Estudiantes</option>
+                                                              <?php while ( $row = $registroe->fetch_array() ){?>
+                                                              <option value=" <?php echo $row['id_usuario'] ?> " >
+                                                              <?php echo $row['nombre_usuario']; ?>
+                                                              </option>
+                                                              <?php }?>    
+                                                          </select> 
+                                                </div>
+                                            </div>
+                                            
+                                            </form>
+
+                                    </div>  
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
         <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
             <div class="navbar-header">
                 <a class="navbar-brand" href="#">Notas Estudiante</a>
